a1aae01d807c29f03f2121651601612aaee58f0f
sheikhnasir75 <115852688+sheikhnasir75@users.noreply.github.com>
2022-12-06 02:21:14 -0800

    Chapter 3
    Chapter 3

---
+++ src/app/model/product-quantity-change.spec.ts
@@ -0,0 +1,7 @@
+import { ProductQuantityChange } from './product-quantity-change';
+
+describe('ProductQuantityChange', () => {
+  it('should create an instance', () => {
+    expect(new ProductQuantityChange()).toBeTruthy();
+  });
+});
---
+++ src/app/products/product-item/product-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit,Input,Output,EventEmitter,ChangeDetectionStrategy } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { ProductQuantityChange } from 'src/app/model/product-quantity-change';
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductItemComponent {
+  @Input()  public product!: Product;
+  @Output() private quantityChange: EventEmitter<ProductQuantityChange> = new EventEmitter();
+
+  constructor() { }
+
+  incrementInCart() {
+    this.quantityChange.emit({product: this.product, changeInQuantity: 1});
+  }
+
+  decrementInCart() {
+    if (this.product.quantityInCart > 0) {
+      this.quantityChange.emit({product: this.product, changeInQuantity: -1});
+    }
+  }
+
+}
